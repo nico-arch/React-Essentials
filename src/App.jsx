e3cde3449bb85ad002bf096a0d14e8079c3d045c
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-06-27 17:41:51 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import ReactImg from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function GenRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={ReactImg} alt="Stylized atomm" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescription[GenRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="./src/assets/react-core-concepts.png" alt="Stylized atomm" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
